0e4c5b377ab559b8f23d582a8aba8ff19127ade3
Buddy Agyin <agyin3@gmail.com>
2020-04-27 10:32:24 -0600

    initial commit

---
+++ data/dbConfig.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+const knex = require('knex')
+const env = process.env.DB_ENV || "development"
+const knexConfig = require("../knexfile")[env]
+
+module.exports = knex(knexConfig)
